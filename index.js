7a9829724c6affc14f31d568ca49a4d6551a5a3e
Liis Normann <liis.normann@gmail.com>
2022-04-21 01:37:57 +0300

    Refractored server side of the handshake

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-const webSocketsServerPort = 8000;
-const webSocketServer = require('websocket').server;
+// Node.js WebSocket server script
 const http = require('http');
+const WebSocketServer = require('websocket').server;
 
-// Spinning the http server and the websocket server.
 const server = http.createServer();
-server.listen(webSocketsServerPort);
-console.log('listening on port 8000');
+server.listen(8000);
 
-const wsServer = new webSocketServer({
+const wsServer = new WebSocketServer({
     httpServer: server
 });
 
-// I'm maintaining all active connections in this object
-const clients = {};
-
-// This code generates unique userid for everyuser.
-const getUniqueID = () => {
-    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    return s4() + s4() + '-' + s4();
-};
-
 wsServer.on('request', function(request) {
-    const userID = getUniqueID();
-    console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
-    // You can rewrite this part of the code to accept only the requests from allowed origin
     const connection = request.accept(null, request.origin);
-    clients[userID] = connection;
-    console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients))
+
+    connection.on('message', function(message) {
+        console.log('Received Message:', message.utf8Data);
+        connection.sendUTF('Hi this is WebSocket server!');
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log('Client has disconnected.');
+    });
 });
