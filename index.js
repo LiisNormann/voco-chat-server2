b1ade5b508c906860b0d9376b130463433e5afc1
Liis Normann <liis.normann@gmail.com>
2022-05-02 00:00:52 +0300

    Handle client connections and broadcasts

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const WebSocketServer = require('websocket').server;
 
 const server = http.createServer();
 server.listen(8000);
+const messages = [];
 
 const wsServer = new WebSocketServer({
     httpServer: server
@@ -13,10 +14,34 @@ wsServer.on('request', function(request) {
     const connection = request.accept(null, request.origin);
 
     connection.on('message', function(message) {
-        console.log('Received Message:', message.utf8Data);
-        connection.sendUTF('Hi this is WebSocket server!');
+        let data = JSON.parse(message.utf8Data);
+        let msg = data.message ?? "";
+        data.message = msg.substring(1, 100);
+
+        console.log(data);
+
+        switch(data.action) {
+            case 'login':
+                connection.send(JSON.stringify(messages));
+                messages.push({created: new Date(), user: data.user, action: 'login'});
+                wsServer.broadcast(JSON.stringify([{created: new Date(), user: data.user, action: 'login'}]));
+                break;
+            default:
+                messages.push({created: new Date(), user: data.user, action: 'message', message: data.message});
+                wsServer.broadcast(JSON.stringify([{created: new Date(), user: data.user, action: 'message', message: data.message}]));
+                break;
+        }
     });
+
     connection.on('close', function(reasonCode, description) {
-        console.log('Client has disconnected.');
+        messages.push({created: new Date(), user: 'Server', action: 'disconnect'});
+        wsServer.broadcast(JSON.stringify([{created: new Date(), user: 'Server', action: 'disconnect'}]));
     });
 });
+
+wsServer.broadcast = function broadcast(msg) {
+    console.log("sending to all clients: " + msg);
+    wsServer.connections.forEach(function(client) {
+        client.send(msg);
+    });
+}
